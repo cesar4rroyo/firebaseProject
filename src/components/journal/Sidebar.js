a7fb0c864e448d63ad436ac2b921710962482286
cesar4rroyo <cesardavid_09@hotmail.com>
2020-07-21 17:26:01 -0500

    Firebase notes crud

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { JournalEntries } from "./JournalEntries";
 import { useDispatch, useSelector } from "react-redux";
 import { startLogout } from "../../actions/auth";
+import { startNewNote } from "../../actions/notes";
 
 export const Sidebar = () => {
   const { name } = useSelector((state) => state.auth);
@@ -10,6 +11,10 @@ export const Sidebar = () => {
   const handleLogout = () => {
     dispatch(startLogout());
   };
+
+  const handleAddNew = () => {
+    dispatch(startNewNote());
+  }
 
   return (
     <aside className="journal__sidebar">
@@ -22,7 +27,9 @@ export const Sidebar = () => {
           Logout
         </button>
       </div>
-      <div className="journal__new-entry">
+      <div className="journal__new-entry"
+        onClick={handleAddNew}
+      >
         <i className="far fa-calendar-plus fa-5x"></i>
         <a className="mt-5">New entry</a>
       </div>
--- src/types/types.js
+++ src/types/types.js
@@ -6,4 +6,18 @@ export const types = {
   uiRemoveError: "[UI] Remove Error",
   uiStartLogin: "[UI] Start loading",
   uiFinishLogin: "[UI] Finish loading",
+
+  notesAddNew: "[Notes] New note",
+  notesActive: "[Notes] Set active note",
+  notesLoad: "[Notes] Load notes",
+  notesUpdated: "[Notes] Update note",
+  notesFileUrl: "[Notes] Update img url",
+  notesDelete: "[Notes] Delete note",
+  notesLogoutCleaning: "[Notes] Logout Cleaning",
+
+
+
+
+
+
 };
